d6d4ecc6bb16b3bff2e34786b398d88ad6e2340f
mbecker20 <becker.maxh@gmail.com>
2024-01-14 03:10:56 -0800

    0.1.6 working with generics

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,11 +1,17 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Field, Type};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Generics, Type};
 
 #[proc_macro_derive(DefaultBuilder)]
 pub fn derive_builder(input: TokenStream) -> TokenStream {
-  let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
+  let DeriveInput {
+    ident,
+    data,
+    generics,
+    vis,
+    ..
+  } = parse_macro_input!(input as DeriveInput);
 
   let s = match data {
     Data::Struct(s) => s,
@@ -21,6 +27,13 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     Some(builder_struct_field(ident, ty))
   });
 
+  let builder_default_fields = s.fields.iter().filter_map(|Field { ident, .. }| {
+    let Some(ident) = ident else {
+      return None;
+    };
+    Some(builder_default_field(ident))
+  });
+
   let fn_build_fields = s.fields.iter().filter_map(|Field { ident, ty, .. }| {
     let Some(ident) = ident else {
       return None;
@@ -32,17 +45,29 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     let Some(ident) = ident else {
       return None;
     };
-    Some(field_set_fn(ident, ty, &builder_ident))
+    Some(field_set_fn(ident, ty, &builder_ident, &generics))
   });
 
+  let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
   quote! {
-    #[derive(Default)]
-    pub struct #builder_ident {
+    #[automatically_derived]
+    #vis struct #builder_ident #generics {
       #(#builder_struct_fields),*
     }
 
-    impl #builder_ident {
-      pub fn build(self) -> #ident {
+    #[automatically_derived]
+    impl #impl_generics Default for #builder_ident #ty_generics {
+      fn default() -> #builder_ident #ty_generics {
+        #builder_ident {
+          #(#builder_default_fields),*
+        }
+      }
+    }
+
+    #[automatically_derived]
+    impl #impl_generics #builder_ident #ty_generics #where_clause {
+      pub fn build(self) -> #ident #ty_generics {
         #ident {
           #(#fn_build_fields),*
         }
@@ -60,16 +85,28 @@ fn builder_struct_field(ident: &Ident, ty: &Type) -> proc_macro2::TokenStream {
   }
 }
 
+fn builder_default_field(ident: &Ident) -> proc_macro2::TokenStream {
+  quote! {
+    #ident: Default::default()
+  }
+}
+
 fn fn_build_field(ident: &Ident, ty: &Type) -> proc_macro2::TokenStream {
   quote! {
     #ident: derive_default_builder::value_maybe_as_option!(#ty, self.#ident.unwrap_or_default(), self.#ident)
   }
 }
 
-fn field_set_fn(ident: &Ident, ty: &Type, builder_ident: &Ident) -> proc_macro2::TokenStream {
+fn field_set_fn(
+  ident: &Ident,
+  ty: &Type,
+  builder_ident: &Ident,
+  generics: &Generics,
+) -> proc_macro2::TokenStream {
   let fn_name = Ident::new(&format!("set_{ident}"), Span::call_site());
+  let (_, ty_generics, _) = generics.split_for_impl();
   quote! {
-    pub fn #fn_name(mut self, #ident: impl Into<#ty>) -> #builder_ident {
+    pub fn #fn_name(mut self, #ident: impl Into<#ty>) -> #builder_ident #ty_generics {
       self.#ident = derive_default_builder::value_as_option!(#ty, #ident.into());
       self
     }
