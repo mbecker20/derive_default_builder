e7bee5e8d35102c3068b1d1a8001cbb49fea53c7
mbecker20 <becker.maxh@gmail.com>
2024-01-14 01:53:01 -0800

    0.1.3 hopefully fix

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Field};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Type};
 
-#[proc_macro_derive(DefaultBuilder, attributes())]
+#[proc_macro_derive(DefaultBuilder)]
 pub fn derive_builder(input: TokenStream) -> TokenStream {
   let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
 
@@ -18,34 +18,21 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     let Some(ident) = ident else {
       return None;
     };
-    let builder_struct_field = quote! {
-      #ident: derive_default_builder::make_option!(#ty)
-    };
-    Some(builder_struct_field)
+    Some(builder_struct_field(ident, ty))
   });
 
-  let fn_build_fields = s.fields.iter().filter_map(|Field { ident, .. }| {
+  let fn_build_fields = s.fields.iter().filter_map(|Field { ident, ty, .. }| {
     let Some(ident) = ident else {
       return None;
     };
-    let fn_build_line = quote! {
-      #ident: self.#ident.unwrap_or_default(),
-    };
-    Some(fn_build_line)
+    Some(fn_build_field(ident, ty))
   });
 
   let field_set_fns = s.fields.iter().filter_map(|Field { ident, ty, .. }| {
     let Some(ident) = ident else {
       return None;
     };
-    let fn_name = Ident::new(&format!("set_{ident}"), Span::call_site());
-    let field_set_fn = quote! {
-      pub fn #fn_name(&mut self, #ident: impl Into<#ty>) -> #builder_ident {
-        self.#ident = Some(#ident.into());
-        self
-      }
-    };
-    Some(field_set_fn)
+    Some(field_set_fn(ident, ty, &builder_ident))
   });
 
   quote! {
@@ -65,4 +52,26 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     }
   }
   .into()
+}
+
+fn builder_struct_field(ident: &Ident, ty: &Type) -> proc_macro2::TokenStream {
+  quote! {
+    #ident: derive_default_builder::make_option!(#ty)
+  }
+}
+
+fn fn_build_field(ident: &Ident, ty: &Type) -> proc_macro2::TokenStream {
+  quote! {
+    #ident: derive_default_builder::value_maybe_as_option!(#ty, self.#ident.unwrap_or_default(), self.#ident)
+  }
+}
+
+fn field_set_fn(ident: &Ident, ty: &Type, builder_ident: &Ident) -> proc_macro2::TokenStream {
+  let fn_name = Ident::new(&format!("set_{ident}"), Span::call_site());
+  quote! {
+    pub fn #fn_name(self, #ident: impl Into<#ty>) -> #builder_ident {
+      self.#ident = derive_default_builder::value_as_option!(#ty, #ident.into());
+      self
+    }
+  }
 }
